23f06984f10a5f0a600a408f0f6c24a967f508a2
Kkkiittt <khamidov357@gmail.com>
2025-10-29 12:18:00 +0500

    init

---
+++ Analite.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace Analite.Domain.Entities;
+
+public class Customer
+{
+	public Guid Id{ get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Surname { get; set; } = string.Empty;
+	public string Email{ get; set; } = string.Empty;
+	public string PasswordHash{ get; set; } = string.Empty;
+	public string SecurityStamp { get; set; } = string.Empty;
+	public DateTime CreatedAt { get; set; }
+	public DateTime UpdatedAt{ get; set; }
+	public bool IsApproved{ get; set; }
+	public bool IsActive{ get; set; }
+}
