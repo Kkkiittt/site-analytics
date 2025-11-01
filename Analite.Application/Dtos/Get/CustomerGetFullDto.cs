eac77526ea5f02a820b121b16103823f430a6721
Kkkiittt <khamidov357@gmail.com>
2025-11-01 14:13:51 +0500

    admin dto

---
+++ Analite.Application/Dtos/Get/CustomerGetFullDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Analite.Application.Dtos.Get;
+
+public class CustomerGetFullDto : CustomerGetDto
+{
+	public string SecurityStamp { get; set; } = string.Empty;
+	public DateTime UpdatedAt { get; set; }
+	public bool IsApproved { get; set; }
+	public bool IsActive { get; set; }
+}
