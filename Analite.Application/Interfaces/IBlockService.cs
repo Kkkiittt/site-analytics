fc450ce350fecf18aa4d71208946e9c020507cc4
Kkkiittt <khamidov357@gmail.com>
2025-11-09 19:09:15 +0500

    minor renaming

--- Analite.Application/Interfaces/IBlockService.cs
+++ Analite.Application/Interfaces/IBlockService.cs
@@ -1,16 +1,14 @@
 using Analite.Application.Dtos.Create;
 using Analite.Application.Dtos.Get;
-using Analite.Domain.Entities;
 
 namespace Analite.Application.Interfaces;
 
 public interface IBlockService
 {
-    Task<BlockGetDto> CreateBlockAsync(BlockCreateDto blockCreateDto);
-    Task<BlockGetDto> UpdateBlockAsync(long id , BlockCreateDto blockCreateDto);
-    Task<BlockGetDto> DeleteBlockAsync(long id);
+	Task<BlockGetDto> CreateBlockAsync(BlockCreateDto blockCreateDto);
+	Task<BlockGetDto> UpdateBlockAsync(long id, BlockCreateDto blockCreateDto);
+	Task DeleteBlockAsync(long id);
 
-    Task<BlockGetDto?> GetByIdAsync(long id);
-    Task<IEnumerable<BlockGetDto>> GetAllAsync(long pageId);
-    
+	Task<BlockGetDto?> GetByIdAsync(long id);
+	Task<IEnumerable<BlockGetDto>> GetByPageAsync(long pageId);
 }
--- Analite.Application/Interfaces/IPageService.cs
+++ Analite.Application/Interfaces/IPageService.cs
@@ -1,18 +1,17 @@
 using Analite.Application.Dtos.Create;
 using Analite.Application.Dtos.Get;
-using Analite.Domain.Entities;
 
 namespace Analite.Application.Interfaces;
 
 public interface IPageService
 {
-    Task<PageGetDto> CreatePageAsync(PageCreateDto pageCreateDto);
-    Task<PageGetDto> UpdatePageAsync(long id , PageCreateDto pageCreateDto);
-    Task<PageGetDto> DeletePageAsync(long id);
-    
-    Task<PageGetDto?> GetByIdAsync(long id);
-    Task<IEnumerable<PageGetDto>> GetAllAsync(Guid customerId);
-    
-    Task<int> GetVisitsCountsAsync(long pageId);
-    Task<int> GetUniqueUsersCountsAsync(long pageId);
+	Task<PageGetDto> CreatePageAsync(PageCreateDto pageCreateDto);
+	Task<PageGetDto> UpdatePageAsync(long id, PageCreateDto pageCreateDto);
+	Task DeletePageAsync(long id);
+
+	Task<PageGetDto?> GetByIdAsync(long id);
+	Task<IEnumerable<PageGetDto>> GetByCustomerAsync(Guid customerId);
+
+	Task<int> GetVisitsCountsAsync(long pageId);
+	Task<int> GetUniqueUsersCountsAsync(long pageId);
 }
