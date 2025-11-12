482cb5326b296e8c22fc975cc453a6e0eb484f76
Kkkiittt <khamidov357@gmail.com>
2025-11-12 14:12:10 +0500

    event contr

---
+++ Analite.Api/Controllers/EventController.cs
@@ -0,0 +1,27 @@
+﻿using Analite.Application.Dtos.Create;
+using Analite.Application.Interfaces;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Analite.Api.Controllers;
+
+[ApiController]
+[Route("events")]
+public class EventController : ControllerBase
+{
+	private readonly IEventService _serv;
+
+	public EventController(IEventService serv)
+	{
+		_serv = serv;
+	}
+
+	[HttpPost]
+	[AllowAnonymous]
+	public async Task<IActionResult> Collect(EventCreateDto dto)
+	{
+		await _serv.CollectAsync(dto);
+		return NoContent();
+	}
+}
